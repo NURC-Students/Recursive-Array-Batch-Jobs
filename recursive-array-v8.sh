0b79c8df66d709c2d6dde14f7442c5bec81aff40
manasvita <github@manasvita.com>
2021-05-06 15:32:11 -0400

    Took out output & error file generating commands from batch header. Redirected screen output from the science section to a screen.out file.  variable is no longer required and renamed /work/rc/m.joshi/stephen-intille-script/Recursive-Array-Batch-Jobs to WORKDIR.

---
+++ recursive-array-v8.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# This script submits an array job on a single node with the highest
+# array index job launching the next set of job recursively.
+
+# Usage:
+#   sbatch recursive-array.sh multiplier-initial-value multiplier-max-limit
+#   For example:
+#   sbatch recursive-array.sh 0 3
+
+#SBATCH --partition=express
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:10:00
+#SBATCH --mem-per-cpu=100M
+#SBATCH --array=[0-9]
+
+# Not redirecting the output & error to corresponding files but can be
+# done if desired.
+
+# To keep track of Iteration, create a counter
+MULTIPLIER=$1
+if [ -z "$MULTIPLIER" ]
+then
+  MULTIPLIER=0
+fi
+
+#PARENT_DIR="$(dirname $PWD)"
+WORKDIR=$PWD
+echo "Main directory for recursive-array script is: $PARENT_DIR"
+
+# Create a directory corresponding to each iteration                                                                                                                                                                                                                                                                                                            
+# number, Slurm Job ID, & Slurm Array Index.
+DIRNUM=$MULTIPLIER-$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID
+mkdir d$DIRNUM
+
+# Switch to that directory, capture screen output, and execute your
+# science there
+cd d$DIRNUM
+echo "d$DIRNUM" >> screen.out
+
+echo "Multiplier is $MULTIPLIER" >> screen.out
+
+# Determine my task-id
+MY_TASK=$((MULTIPLIER * 10 + SLURM_ARRAY_TASK_ID))
+
+echo "Job array ID: $SLURM_ARRAY_JOB_ID , sub-job $SLURM_ARRAY_TASK_ID is running!" >> screen.out
+echo "Highest job array index value is $SLURM_ARRAY_TASK_MAX" >> screen.out
+echo "MY_TASK is $MY_TASK" >> screen.out
+
+# Science happens here
+for ((k = 0; k < 3; k++)); do 
+    python $PARENT_DIR/test_function_script-mj.py > test-array-express-$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID-$k.out
+
+done 
+
+# Switch back to the parent directory
+cd ..
+
+# Quit when the counter reaches user-specified limit 
+USERLIMIT=$2
+if [ -z "$USERLIMIT" ]
+then
+    USERLIMIT=3
+fi
+
+echo "USERLIMIT is $USERLIMIT"
+
+if [ $MULTIPLIER -eq $USERLIMIT ]
+then
+  exit
+fi
+
+# Put to sleep for few seconds
+SLEEPVAR=$(( $RANDOM % 5 + 1 ))
+echo "sleep time is $SLEEPVAR" 
+
+sleep $SLEEPVAR
+
+# Command to get pending array jobs 
+CMD='squeue -u $USER --jobs=$SLURM_ARRAY_JOB_ID -t pd -h | wc -l'
+echo "executing command: $CMD"
+
+PDJOBS=$(squeue -u $USER --jobs=$SLURM_ARRAY_JOB_ID -t pd -h | wc -l)
+echo "Pending Jobs are $PDJOBS"
+
+# Start next generation of iterations only when array index matches
+# the highest array index
+if [ $SLURM_ARRAY_TASK_ID -eq $SLURM_ARRAY_TASK_MAX ]
+then
+   echo "Continuing Next Iteration"
+   sbatch recursive-array-v8.sh $((MULTIPLIER + 1)) $USERLIMIT
+fi
