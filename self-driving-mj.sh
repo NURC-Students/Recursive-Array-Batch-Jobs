dabcfe48206360f06a9dafe45396b19ae504ad75
manasvita <github@manasvita.com>
2021-05-06 15:50:55 -0400

    Edited few commands where math in bash was not included correctly.

---
+++ self-driving-mj.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH --partition=debug
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=00:00:2
+#SBATCH --array=[0-9]%2
+
+MULTIPLIER=$1
+
+if [ -z "$MULTIPLIER" ]
+then
+  MULTIPLIER=0
+fi
+
+# determine my real task-id
+MY_TASK=$((MULTIPLIER * 10 + SLURM_ARRAY_TASK_ID ))
+
+echo "This is where science happens"
+
+echo "MY_TASK is $MY_TASK"
+
+# quit when you have had enough 
+if [ "$MULTIPLIER" == "3" ]
+then
+  exit
+fi
+
+# but sometimes you want more
+sleep $((RANDOM % 5 ) + 1)s
+MOAR=$(squeue -u gshomo -t pd -h | wc -l )
+if [ $MOAR -eq 0 ]
+then
+   echo "making moar"
+   sbatch self-driving-array-test.sh $((MULTIPLIER + 1))
+fi
+
